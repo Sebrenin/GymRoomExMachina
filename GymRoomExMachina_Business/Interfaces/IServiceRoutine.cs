0fc703cd4ad649c5db815a43b3a5a1ee8744cfe0
gatonegro999 <21020645@cobachih.edu.mx>
2025-11-20 08:54:09 -0600

    animo gays

---
+++ GymRoomExMachina_Business/Interfaces/IServiceRoutine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymRoom_Business.Interfaces
+{
+    //public interface IServiceRoutine
+    //{
+    //    public void AddRoutine(CatRoutine routine);
+    //    public void EditRoutine(CatRoutine routine);
+    //    public void DeleteRoutine(CatRoutine routine);
+    //    public List<CatRoutine> GetAllRoutines();
+    //    public List<CatRoutine> SearchRoutines(string routine);
+    //}
+}
---
+++ GymRoomExMachina_Business/Services/ServiceRoutine.cs
@@ -0,0 +1,67 @@
+﻿using GymRoom_Business.Interfaces;
+using GymRoom_Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymRoom_Business.Services
+{
+    //public class ServiceRoutine : IServiceRoutine
+    //{
+    //    private readonly IRepositoryRoutine _repositoryRoutine;
+
+    //    public ServiceRoutine(IRepositoryRoutine repositoryRoutine)
+    //    {
+    //        _repositoryRoutine = repositoryRoutine;
+    //    }
+
+    //    public void AddRoutine(CatRoutine routine)
+    //    {
+    //        try
+    //        {
+    //            _repositoryRoutine.AddRoutine(routine);
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            throw;
+    //        }
+    //    }
+
+    //    public void EditRoutine(CatRoutine routine)
+    //    {
+    //        try
+    //        {
+    //            _repositoryRoutine.EditRoutine(routine);
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            throw;
+    //        }
+    //    }
+
+    //    public void DeleteRoutine(CatRoutine routine)
+    //    {
+    //        try
+    //        {
+    //            _repositoryRoutine.DeleteRoutine(routine);
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            throw;
+    //        }
+    //    }
+
+    //    public List<CatRoutine> GetAllRoutines()
+    //    {
+    //        return _repositoryRoutine.GetAllRoutines();
+    //    }
+
+    //    public List<CatRoutine> SearchRoutines(string search)
+    //    {
+    //        return _repositoryRoutine.SearchRoutines(search);
+    //    }
+    //}
+}
+
