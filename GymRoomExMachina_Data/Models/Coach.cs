fa2800c249512418dbf9ce164b8bd6012b8ccb66
Sebrenin Anghofiedig <Sebasstian.fernandez@gmail.com>
2025-11-20 21:14:48 -0600

    Subida de GymRoomExMachina_Data

---
+++ GymRoomExMachina_Data/Models/Coach.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GymRoomExMachina_Data.Models;
+
+public partial class Coach
+{
+    public int IdCoach { get; set; }
+
+    public string NameCoach { get; set; } = null!;
+
+    public bool Active { get; set; }
+
+    public string LogInCoach { get; set; } = null!;
+
+    public string PasswordCoach { get; set; } = null!;
+
+    public virtual ICollection<Member> Members { get; set; } = new List<Member>();
+}
---
+++ GymRoomExMachina_Data/Models/Member.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GymRoomExMachina_Data.Models;
+
+public partial class Member
+{
+    public int IdMember { get; set; }
+
+    public string NameMember { get; set; } = null!;
+
+    public bool Active { get; set; }
+
+    public decimal? Weight { get; set; }
+
+    public decimal? Height { get; set; }
+
+    public string LogInMember { get; set; } = null!;
+
+    public string PasswordMember { get; set; } = null!;
+
+    public DateTime SignUpDate { get; set; }
+
+    public int? IdRoutine { get; set; }
+
+    public int? IdCoach { get; set; }
+
+    public string? Goal { get; set; }
+
+    public string? BodyType { get; set; }
+
+    public int? Age { get; set; }
+
+    public decimal? FatPct { get; set; }
+
+    public decimal? FatKg { get; set; }
+
+    public decimal? MuscleKg { get; set; }
+
+    public decimal? Tricep { get; set; }
+
+    public decimal? Subscapular { get; set; }
+
+    public decimal? Bicep { get; set; }
+
+    public decimal? Iliocrestal { get; set; }
+
+    public decimal? Waist { get; set; }
+
+    public virtual Coach? IdCoachNavigation { get; set; }
+
+    public virtual Routine? IdRoutineNavigation { get; set; }
+}
