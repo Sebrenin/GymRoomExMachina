7767d5cd584f17a3153e9ce5de7db754cbf413b5
gatonegro999 <21020645@cobachih.edu.mx>
2025-11-23 05:43:22 -0600

    mis cambios para hacer el pull sin errores

--- frmHome.Designer.cs
+++ frmHome.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class frmHome
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +15,6 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHome));
@@ -33,35 +22,23 @@
             panel1 = new Panel();
             panel3 = new Panel();
             pictureBox1 = new PictureBox();
-            label2 = new Label();
-            panel2 = new Panel();
-            quicknote = new RichTextBox();
-            gotonotes = new PictureBox();
-            pictureBox2 = new PictureBox();
-            label3 = new Label();
             profile = new PictureBox();
             chats = new PictureBox();
             members = new PictureBox();
-<<<<<<< Updated upstream
-            label7 = new Label();
-            label8 = new Label();
-            label6 = new Label();
-            label5 = new Label();
-            pictureBox10 = new PictureBox();
-            pictureBox9 = new PictureBox();
-=======
->>>>>>> Stashed changes
+            gotonotes = new PictureBox();
+            label3 = new Label();
+            pictureBox2 = new PictureBox();
+            panel2 = new Panel();
+            wrtNote1 = new GymRoomExMachina.wrtNote();
             panel1.SuspendLayout();
             panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)gotonotes).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)profile).BeginInit();
             ((System.ComponentModel.ISupportInitialize)chats).BeginInit();
             ((System.ComponentModel.ISupportInitialize)members).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)gotonotes).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -79,14 +56,7 @@
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(217, 217, 217);
-            panel1.Controls.Add(label7);
             panel1.Controls.Add(panel3);
-            panel1.Controls.Add(label8);
-            panel1.Controls.Add(label2);
-            panel1.Controls.Add(label6);
-            panel1.Controls.Add(label5);
-            panel1.Controls.Add(pictureBox9);
-            panel1.Controls.Add(pictureBox10);
             panel1.Location = new Point(61, 50);
             panel1.Margin = new Padding(3, 2, 3, 2);
             panel1.Name = "panel1";
@@ -114,75 +84,6 @@
             pictureBox1.TabIndex = 8;
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Century Gothic", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(19, 14);
-            label2.Name = "label2";
-            label2.Size = new Size(135, 16);
-            label2.TabIndex = 0;
-            label2.Text = "Your recent messages";
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.FromArgb(217, 217, 217);
-            panel2.Controls.Add(quicknote);
-            panel2.Controls.Add(gotonotes);
-            panel2.Controls.Add(pictureBox2);
-            panel2.Controls.Add(label3);
-            panel2.Location = new Point(61, 203);
-            panel2.Margin = new Padding(3, 2, 3, 2);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(612, 115);
-            panel2.TabIndex = 2;
-            // 
-            // quicknote
-            // 
-            quicknote.BorderStyle = BorderStyle.None;
-            quicknote.Location = new Point(15, 37);
-            quicknote.Name = "quicknote";
-            quicknote.Size = new Size(585, 68);
-            quicknote.TabIndex = 0;
-            quicknote.Text = "";
-<<<<<<< Updated upstream
-=======
-            //quicknote.TextChanged += quicknote_TextChanged;
->>>>>>> Stashed changes
-            // 
-            // gotonotes
-            // 
-            gotonotes.Image = (Image)resources.GetObject("gotonotes.Image");
-            gotonotes.Location = new Point(15, 8);
-            gotonotes.Name = "gotonotes";
-            gotonotes.Size = new Size(25, 22);
-            gotonotes.SizeMode = PictureBoxSizeMode.Zoom;
-            gotonotes.TabIndex = 5;
-            gotonotes.TabStop = false;
-            gotonotes.Click += gotonotes_Click;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(577, 8);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(20, 20);
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox2.TabIndex = 5;
-            pictureBox2.TabStop = false;
-            pictureBox2.Click += pictureBox2_Click;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Century Gothic", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(59, 14);
-            label3.Name = "label3";
-            label3.Size = new Size(59, 16);
-            label3.TabIndex = 3;
-            label3.Text = "My notes";
-            label3.Click += label3_Click;
             // 
             // profile
             // 
@@ -217,71 +118,61 @@
             members.TabStop = false;
             members.Click += pictureBox6_Click;
             // 
-<<<<<<< Updated upstream
-            // label7
+            // gotonotes
             // 
-            label7.AutoSize = true;
-            label7.Font = new Font("Segoe UI", 7.20000029F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label7.Location = new Point(58, 77);
-            label7.Name = "label7";
-            label7.Size = new Size(116, 12);
-            label7.TabIndex = 20;
-            label7.Text = "I want to change my diet";
+            gotonotes.Image = (Image)resources.GetObject("gotonotes.Image");
+            gotonotes.Location = new Point(15, 8);
+            gotonotes.Name = "gotonotes";
+            gotonotes.Size = new Size(25, 22);
+            gotonotes.SizeMode = PictureBoxSizeMode.Zoom;
+            gotonotes.TabIndex = 5;
+            gotonotes.TabStop = false;
+            gotonotes.Click += gotonotes_Click;
             // 
-            // label8
+            // label3
             // 
-            label8.AutoSize = true;
-            label8.Font = new Font("Segoe UI", 7.20000029F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label8.Location = new Point(58, 64);
-            label8.Name = "label8";
-            label8.Size = new Size(38, 12);
-            label8.TabIndex = 19;
-            label8.Text = "Ryan G";
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(46, 12);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 16);
+            label3.TabIndex = 3;
+            label3.Text = "My notes";
+            label3.Click += label3_Click;
             // 
-            // label6
+            // pictureBox2
             // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 7.20000029F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label6.Location = new Point(59, 43);
-            label6.Name = "label6";
-            label6.Size = new Size(58, 12);
-            label6.TabIndex = 18;
-            label6.Text = "I'll arrive late";
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(577, 8);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(20, 20);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
             // 
-            // label5
+            // panel2
             // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 7.20000029F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(59, 30);
-            label5.Name = "label5";
-            label5.Size = new Size(48, 12);
-            label5.TabIndex = 17;
-            label5.Text = "John Doe";
+            panel2.BackColor = Color.FromArgb(217, 217, 217);
+            panel2.Controls.Add(wrtNote1);
+            panel2.Controls.Add(gotonotes);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(label3);
+            panel2.Location = new Point(61, 203);
+            panel2.Margin = new Padding(3, 2, 3, 2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(612, 115);
+            panel2.TabIndex = 2;
             // 
-            // pictureBox10
+            // wrtNote1
             // 
-            pictureBox10.BackgroundImage = (Image)resources.GetObject("pictureBox10.BackgroundImage");
-            pictureBox10.BackgroundImageLayout = ImageLayout.Zoom;
-            pictureBox10.Location = new Point(30, 67);
-            pictureBox10.Margin = new Padding(3, 2, 3, 2);
-            pictureBox10.Name = "pictureBox10";
-            pictureBox10.Size = new Size(23, 22);
-            pictureBox10.TabIndex = 16;
-            pictureBox10.TabStop = false;
+            wrtNote1.BackColor = Color.FromArgb(192, 190, 190);
+            wrtNote1.CurrentNote = null;
+            wrtNote1.Location = new Point(15, 37);
+            wrtNote1.Name = "wrtNote1";
+            wrtNote1.Size = new Size(582, 68);
+            wrtNote1.TabIndex = 6;
             // 
-            // pictureBox9
-            // 
-            pictureBox9.BackgroundImage = (Image)resources.GetObject("pictureBox9.BackgroundImage");
-            pictureBox9.BackgroundImageLayout = ImageLayout.Zoom;
-            pictureBox9.Location = new Point(30, 31);
-            pictureBox9.Margin = new Padding(3, 2, 3, 2);
-            pictureBox9.Name = "pictureBox9";
-            pictureBox9.Size = new Size(23, 22);
-            pictureBox9.TabIndex = 15;
-            pictureBox9.TabStop = false;
-            // 
-=======
->>>>>>> Stashed changes
             // frmHome
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -294,23 +185,19 @@
             Controls.Add(panel2);
             Controls.Add(panel1);
             Controls.Add(label1);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "frmHome";
             Text = "frmHome";
             Load += frmHome_Load;
             panel1.ResumeLayout(false);
-            panel1.PerformLayout();
             panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            panel2.ResumeLayout(false);
-            panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)gotonotes).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)profile).EndInit();
             ((System.ComponentModel.ISupportInitialize)chats).EndInit();
             ((System.ComponentModel.ISupportInitialize)members).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)gotonotes).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -319,22 +206,15 @@
 
         private Label label1;
         private Panel panel1;
-        private Label label2;
-        private Panel panel2;
         private Panel panel3;
-        private Label label3;
-        private PictureBox gotonotes;
-        private PictureBox pictureBox2;
         private PictureBox profile;
         private PictureBox chats;
         private PictureBox members;
         private PictureBox pictureBox1;
-        private RichTextBox quicknote;
-        private Label label7;
-        private Label label8;
-        private Label label6;
-        private Label label5;
-        private PictureBox pictureBox9;
-        private PictureBox pictureBox10;
+        private PictureBox gotonotes;
+        private Label label3;
+        private PictureBox pictureBox2;
+        private Panel panel2;
+        private GymRoomExMachina.wrtNote wrtNote1;
     }
 }
